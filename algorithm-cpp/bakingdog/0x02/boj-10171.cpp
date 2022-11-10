1356dbd7cc90493626b5753a19e81e3259c59d81
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-10 10:05:20 +0900

    feat($compile): 바킹독 알고리즘 기초 2강

---
+++ algorithm-cpp/bakingdog/0x02/boj-10171.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "\\    /\\" << "\n";
+    cout << " )  ( ')" << "\n";
+    cout << "(  /  )" << "\n";
+    cout << " \\(__)|" << "\n";
+}
